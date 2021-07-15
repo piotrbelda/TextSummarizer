9b4e613e13fdbf15c90a5fa133b0729cc9faa16c
piotrek <piotrek.belda@wp.pl>
2021-07-15 20:11:38 +0200

    code coverage and quality

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,8 +1,8 @@
 # project/app/api/crud.py
-from typing import Union
+from typing import List, Union
+
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
-from typing import Union, List
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
@@ -13,11 +13,13 @@ async def post(payload: SummaryPayloadSchema) -> int:
     await summary.save()
     return summary.id
 
+
 async def get(id: int) -> Union[dict, None]:
     summary = await TextSummary.filter(id=id).first().values()
     if summary:
         return summary[0]
     return None
+
 
 async def get_all() -> List:
     summaries = await TextSummary.all().values()
--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,10 +1,11 @@
 import os
-from tortoise.contrib.fastapi import register_tortoise
+
 import pytest
 from starlette.testclient import TestClient
+from tortoise.contrib.fastapi import register_tortoise
 
+from app.config import Settings, get_settings
 from app.main import create_application
-from app.config import get_settings, Settings
 
 
 def get_settings_override():
